a880fd5e061a70235848e05b3165ca9c4f465515
Seu Nome <seuemail@exemplo.com>
2024-11-07 22:27:51 -0300

    full version

--- model/script.js
+++ model/script.js
@@ -41,3 +41,43 @@ window.onload = function() {
     type();
 };
 
+
+emailjs.init('ytaQzSYC8qgU4eKkI'); 
+
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+  event.preventDefault(); 
+
+  
+  emailjs.sendForm('service_smji11o', 'template_gz2lhd8', this) 
+    .then(function(response) {
+      console.log('SUCCESS!', response);
+      
+      
+      document.getElementById('toast-message').textContent = 'Mensagem enviada com sucesso!';
+      document.getElementById('toast').classList.remove('hidden', 'bg-red-500');
+      document.getElementById('toast').classList.add('bg-green-500');
+      
+      
+      document.getElementById('contact-form').reset(); 
+
+      
+      setTimeout(function() {
+        document.getElementById('toast').classList.add('hidden');
+      }, 3000);
+    }, function(error) {
+      console.log('FAILED...', error);
+
+      
+      document.getElementById('toast-message').textContent = 'Falha ao enviar mensagem. Tente novamente.';
+      document.getElementById('toast').classList.remove('hidden', 'bg-green-500');
+      document.getElementById('toast').classList.add('bg-red-500');
+
+      
+      setTimeout(function() {
+        document.getElementById('toast').classList.add('hidden');
+      }, 3000);
+    });
+});
+
+
